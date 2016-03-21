ea38b437ea082760c9961eea35ed7329c6b35a66
Tom Miller <thomas.miller2@nrel.gov>
2016-03-21 14:08:38 -0600

    Fully working version of exporter.py that creates an s3 file properly after getting all the unpacked orders.

--- exporter.py
+++ exporter.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
-import os
 import datetime
 import json
-from pprint import pprint
+import boto3
 from tradegecko import TradeGeckoRestClient
 
+# Use s3 bucket to get access token for the API and prepare output documents
+myBucket = 'fosdick-exports'
+s3 = boto3.resource('s3')
+s3c = boto3.client('s3')
+object = s3.Object(myBucket,'.tg')
+access_token = object.get()["Body"].read().decode("utf-8")
+
+# Set up some simple variables for usage throughout
 now = datetime.datetime.today()
 today = now.strftime('%Y%m%d')
 time = now.strftime('%H%M%S')
-outfile = "packed_" + today + ".tsv"
-tg = TradeGeckoRestClient(os.environ.get("TRADEGECKO_ACCESS_TOKEN", None))
+outdir = "/tmp"
+outfilename = "packed_" + today + "_" + time + ".tsv"
+outfile = outdir + "/" + outfilename
 
-# We don't need json.loads or json.dumps because we want this as a python list to be able to use it
 # Get all orders that have a packed_status of "unpacked"
+tg = TradeGeckoRestClient(access_token.rstrip())
 orders = tg.order.filter(packed_status="unpacked")
 totalOrders = orders['meta']['total']
 
@@ -82,7 +90,9 @@ with open(outfile, 'w') as f:
                   "\t" + "\t" + \
                   "\t" + "\t\n"
       f.write(orderLine)
-   trailerLine = "Trailer Record\t" + str(outfile) + "\t" + str(today) + "\t" + str(time) + "\t" + str(totalOrders) + "\t" + "00001\n" 
+   trailerLine = "Trailer Record\t" + str(outfilename) + "\t" + str(today) + "\t" + str(time) + "\t" + str(totalOrders) + "\t" + "00001\n" 
    f.write(trailerLine)
 
-# Update packed_status of orders to packed when we send .tsv to fosdick
+def lambda_handler(event, context):
+   s3c.upload_file(outfile, myBucket, outfilename)
+   return "File successfully created s3://" + myBucket + "/" + outfilename + "."
